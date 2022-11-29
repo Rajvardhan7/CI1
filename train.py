06e73b50a0114a45b6dad9a14c5dec5dc70340c0
Rajvardhan7 <45497037+Rajvardhan7@users.noreply.github.com>
2022-11-29 17:16:31 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,9 +1,10 @@
 import pandas as pd 
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+
 # Set random seed
 seed = 42
 
